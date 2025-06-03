c3be7c88eb5fef02be93be8944f442c3d5018613
AnilBASARAN <nlbsrn@gmail.com>
2025-06-04 02:07:13 +0300

    we registered all components in router.js

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="titleMe">
+    <h2>Hey dude</h2>
+  </div>
+</template>
+<style scoped>
+.titleMe {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: gainsboro;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachesRegistation from './pages/coaches/CoachRegistation.vue';
+import NotFound from './pages/NotFound.vue';
+import RequestsRecieved from './pages/requests/RequestsReceived.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
+      component: CoachDetail,
       children: [
-        { path: '/contact', component: null }, // coaches/c1/contact
+        { path: '/contact', component: ContactCoach }, // coaches/c1/contact
       ],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: CoachesRegistation },
+    { path: '/requests', component: RequestsRecieved },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
