480d6a56cedf2edbfeae7e73424fa3faad79d974
AnilBASARAN <nlbsrn@gmail.com>
2025-06-04 15:56:46 +0300

    added the latest

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,8 +5,23 @@
       <button>Refresh</button>
       <router-link to="/register">Register</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hascoaches'];
+    },
+  },
+};
+</script>
